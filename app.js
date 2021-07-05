d67938ff0cc34f77f3234048f67a32c6012a8ec9
NarendraVanka <vanka.narendra@gmail.com>
2021-07-05 11:39:49 +0530

    added book crud apis

---
+++ app.js
@@ -0,0 +1,48 @@
+const {
+    Febby 
+  } = require('febby');
+   
+  const config = {
+    port: 3000,
+    db: {
+      url: 'mongodb://localhost:27017/bookdb'
+    },
+    appBaseUrl: '/booksrv'
+  }
+  const febby = new Febby(config);
+   
+  const api = febby.router('/api'); 
+
+  const books = febby.model('books', {
+    name: {
+      type: String
+    },
+    author: {
+      type: String
+    },
+    price :{
+        type: Number,
+        default: 0
+    }
+  });
+   
+  febby.crud('/books', {
+    crud: true,
+    middlewares: []
+  }, books, api);
+   
+  febby.route({
+    router: api,
+    path: '/',
+    method: 'get',
+    handler: (req, res) => {
+      const message = 'welcome to febby.!';
+      res.json({
+        message
+      })
+    }
+  })
+   
+  febby.bootstrap(() => {
+    console.log(`Server started on port : ${config.port}`)
+  });
